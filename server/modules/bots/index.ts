dc641c01af8acb2e225ec5befb46b4c8aa748309
Kelly Copley <copleykj@gmail.com>
2020-03-10 16:22:58 -0400

    don't announce to slack/twitter if we are a in development

--- server/modules/bots/index.ts
+++ server/modules/bots/index.ts
@@ -42,43 +42,45 @@ const batchAnnouncePackageUpdates = () => {
   }
 };
 
-PackageServer.runIfSyncFinished(() => {
-  if (!Settings.get('lastAnnounceTime')) {
-    Settings.set('lastAnnounceTime', new Date());
-  }
-
-  Meteor.setInterval(batchAnnouncePackageUpdates, ANNOUNCE_INTERVAL);
-
-  LatestPackages.after.update(async (userId: string, doc: LatestPackage) => {
-    const { packageName, lastUpdated, published } = doc;
-    if (lastUpdated.getTime() !== published.getTime()) {
-      const text = `Metadata Update: \`${packageName}\`\n\n${makeAtmosphereLink(packageName)}`;
-
-      postToSlack(text);
-      postTwitterStatus(text);
+if (Meteor.isProduction) {
+  PackageServer.runIfSyncFinished(() => {
+    if (!Settings.get('lastAnnounceTime')) {
+      Settings.set('lastAnnounceTime', new Date());
     }
-  }, {fetchPrevious: false});
 
-  ReleaseVersions.after.insert(async (userId: string, doc: ReleaseVersion) => {
-    const { track, version } = doc;
-    if (track === 'METEOR') {
-      const text = `New Meteor Release: \`${track}@${version}\``;
+    Meteor.setInterval(batchAnnouncePackageUpdates, ANNOUNCE_INTERVAL);
 
-      postToSlack(text);
-      postTwitterStatus(text);
-    }
+    LatestPackages.after.update(async (userId: string, doc: LatestPackage) => {
+      const { packageName, lastUpdated, published } = doc;
+      if (lastUpdated.getTime() !== published.getTime()) {
+        const text = `Metadata Update: \`${packageName}\`\n\n${makeAtmosphereLink(packageName)}`;
+
+        postToSlack(text);
+        postTwitterStatus(text);
+      }
+    }, {fetchPrevious: false});
+
+    ReleaseVersions.after.insert(async (userId: string, doc: ReleaseVersion) => {
+      const { track, version } = doc;
+      if (track === 'METEOR') {
+        const text = `New Meteor Release: \`${track}@${version}\``;
+
+        postToSlack(text);
+        postTwitterStatus(text);
+      }
+    });
+
+    ReleaseVersions.after.update(
+      async function(this: { previous: ReleaseVersion }, userId: string, doc: ReleaseVersion) {
+      const { track, version, recommended} = doc;
+
+      const { recommended: previousRecommend } = this.previous as ReleaseVersion;
+      if (recommended !== previousRecommend && recommended === true) {
+        const text = `\`${track}@${version}\` is now a recommended release. 🎉 \n\nTime to update your apps!`;
+
+        postToSlack(text);
+        postTwitterStatus(text);
+      }
+    });
   });
-
-  ReleaseVersions.after.update(
-    async function(this: { previous: ReleaseVersion }, userId: string, doc: ReleaseVersion) {
-    const { track, version, recommended} = doc;
-
-    const { recommended: previousRecommend } = this.previous as ReleaseVersion;
-    if (recommended !== previousRecommend && recommended === true) {
-      const text = `\`${track}@${version}\` is now a recommended release. 🎉 \n\nTime to update your apps!`;
-
-      postToSlack(text);
-      postTwitterStatus(text);
-    }
-  });
-});
+}
