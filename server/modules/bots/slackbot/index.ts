52e90f4b64a5f7adbf1d6bca4785d252e1256999
Kelly Copley <copleykj@gmail.com>
2021-02-05 16:21:07 -0500

    don't throw errors when there aren't bot tokens

--- server/modules/bots/slackbot/index.ts
+++ server/modules/bots/slackbot/index.ts
@@ -2,27 +2,24 @@ import { WebClient } from '@slack/web-api';
 import { Meteor } from 'meteor/meteor';
 
 const botToken: string = Meteor.settings?.slack?.botToken;
-let client: WebClient;
+let client: WebClient | null = null;
 
 const CHANNEL = '#packages';
 
 if (typeof botToken !== 'undefined' && botToken.length !== 0) {
   client = new WebClient(botToken);
-} else {
-  throw new Meteor.Error(
-    'NoToken',
-    'It appears that you have not specified a slack token in settings.json',
-  );
 }
 
 export const postToSlack = async (text: string, channel: string = CHANNEL): Promise<void> => {
-  try {
-    await client.chat.postMessage({
-      channel,
-      text,
-    });
-  } catch (error) {
-    console.log(error);
+  if (client !== null) {
+    try {
+      await client.chat.postMessage({
+        channel,
+        text,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
--- server/modules/bots/twitterbot/index.ts
+++ server/modules/bots/twitterbot/index.ts
@@ -3,23 +3,20 @@ import { Meteor } from 'meteor/meteor';
 import Twitter, { AccessTokenOptions } from 'twitter';
 
 const twitterOauth: AccessTokenOptions = Meteor.settings?.twitter;
-let client: Twitter;
+let client: Twitter | null = null;
 
 if (typeof twitterOauth !== 'undefined' && Object.keys(twitterOauth).length >= 4) {
   client = new Twitter(twitterOauth);
-} else {
-  throw new Meteor.Error(
-    'NoTwitterSettings',
-    'It seems you may have missed one of the 4 required properties or have forgotten to specify your twitter access all together keys in settings.json',
-  );
-}
+};
 
 export const postTwitterStatus = async (text: string): Promise<void> => {
-  text = text.replace(/(<)|(>)/g, '');
-  try {
-    await client.post('statuses/update', { status: text });
-  } catch (error) {
-    console.log(error);
+  if (client !== null) {
+    text = text.replace(/(<)|(>)/g, '');
+    try {
+      await client.post('statuses/update', { status: text });
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
