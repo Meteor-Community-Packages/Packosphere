27b6c3ac11ff69d42b85bc8f36163c0ac720a0cc
Kelly Copley <copleykj@gmail.com>
2021-02-10 10:21:39 -0500

    segregate pagination for use on other pages

---
+++ imports/ui/components/Pagination/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import queryString from 'query-string';
+import { ArrowNarrowRight, ArrowNarrowLeft, Refresh } from 'heroicons-react';
+import useLocationQuery from '../../hooks/useLocationQuery';
+
+interface PaginationProps {
+  totalPages: number
+  loading: boolean
+}
+
+interface LinkOrNotProps {
+  link: boolean
+  to: string
+  children: any
+  [key: string]: any
+}
+
+const LinkOrNot = ({ link, to, children, ...props }: LinkOrNotProps): JSX.Element => {
+  return link
+    ? <Link to={to} {...props} >{children}</Link>
+    : <span className='text-blueGray-600' {...props}>{children}</span>;
+};
+
+const Pagination = ({ totalPages = 0, loading = false }: PaginationProps): JSX.Element => {
+  const [search] = useLocationQuery();
+  const { page = '1' } = search;
+  const pageNumber = parseInt(page as string);
+
+  const nextPageNumber = pageNumber + 1;
+  const prevPageNumber = pageNumber - 1;
+  const nextSearchString = queryString.stringify({ ...search, page: nextPageNumber.toString() });
+  const prevSearchString = queryString.stringify({ ...search, page: prevPageNumber.toString() });
+  const showLoader = loading ? '' : 'hidden';
+
+  return (
+    <>
+      <span className={`animate-spin rounded-full text-blueGray-400 ${showLoader}`}>
+        <Refresh size={22} className="h-6 w-6" style={{ transform: 'scaleX(-1)' }}/>
+      </span>
+      <span className="flex items-center">
+        <LinkOrNot link={pageNumber !== 1} to={`/search?${prevSearchString}`} title="Previous">
+          <ArrowNarrowLeft size={20} />
+        </LinkOrNot>
+        <span className="w-40 text-center">page {pageNumber} of {totalPages}</span>
+        <LinkOrNot link={pageNumber !== totalPages} to={`/search?${nextSearchString}`} title="Next">
+          <ArrowNarrowRight size={20} />
+        </LinkOrNot>
+      </span>
+    </>
+  );
+};
+
+export default Pagination;
--- imports/ui/pages/Search/index.tsx
+++ imports/ui/pages/Search/index.tsx
@@ -1,56 +1,15 @@
 import React, { useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import queryString from 'query-string';
-import { ArrowNarrowRight, ArrowNarrowLeft, Refresh } from 'heroicons-react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Page from '../../components/Page';
 import CardGrid from '../../components/CardGrid';
+import Pagination from '../../components/Pagination';
 
 import useQuery from '../../hooks/useStaticQuery';
 import { QPackageSearch } from '../../../../api/LatestPackages';
 import useLocationQuery from '../../hooks/useLocationQuery';
 
 const resultsPerPage = 10;
-
-interface PaginationProps {
-  totalPages: number
-}
-
-interface LinkOrNotProps {
-  link: boolean
-  to: string
-  children: any
-  [key: string]: any
-}
-
-const LinkOrNot = ({ link, to, children, ...props }: LinkOrNotProps): JSX.Element => {
-  return link
-    ? <Link to={to} {...props} >{children}</Link>
-    : <span className='text-blueGray-600' {...props}>{children}</span>;
-};
-
-const Pagination = ({ totalPages = 1 }: PaginationProps): JSX.Element => {
-  const [search] = useLocationQuery();
-  const { page = '1' } = search;
-  const pageNumber = parseInt(page as string);
-
-  const nextPageNumber = pageNumber + 1;
-  const prevPageNumber = pageNumber - 1;
-  const nextSearchString = queryString.stringify({ ...search, page: nextPageNumber.toString() });
-  const prevSearchString = queryString.stringify({ ...search, page: prevPageNumber.toString() });
-
-  return (
-    <span className="flex items-center">
-      <LinkOrNot link={pageNumber !== 1} to={`/search?${prevSearchString}`} title="Previous">
-        <ArrowNarrowLeft size={20} />
-      </LinkOrNot>
-      <span className="w-40 text-center">page {pageNumber} of {totalPages}</span>
-      <LinkOrNot link={pageNumber !== totalPages} to={`/search?${nextSearchString}`} title="Next">
-        <ArrowNarrowRight size={20} />
-      </LinkOrNot>
-    </span>
-  );
-};
 
 const SearchPage = (): JSX.Element => {
   const history = useHistory();
@@ -64,7 +23,7 @@ const SearchPage = (): JSX.Element => {
             behavior: 'smooth',
           });
         }
-      });
+      }, 300);
     });
 
     return () => { unlisten(); };
@@ -76,7 +35,6 @@ const SearchPage = (): JSX.Element => {
   const { data, count, loading } = useQuery({ query: QPackageSearch, params: { query: q, limit: resultsPerPage, skip }, config: { fetchTotal: true } });
   const dataCount = typeof count !== 'undefined' ? count : data.length;
   const totalPages = Math.ceil(dataCount / resultsPerPage);
-  const showLoader = loading ? '' : 'hidden';
   return (
     <>
       <Header />
@@ -85,15 +43,12 @@ const SearchPage = (): JSX.Element => {
           <section className="flex justify-between items-center">
             <h2 className="text-xl text-white">Results for <span className="text-yellow-600 italic">{q}</span></h2>
             <aside className="flex items-center space-x-3">
-              <span className={`animate-spin rounded-full text-blueGray-400 ${showLoader}`}>
-                <Refresh size={22} className="h-6 w-6" style={{ transform: 'scaleX(-1)' }}/>
-              </span>
-              <Pagination totalPages={totalPages} />
+              <Pagination totalPages={totalPages} loading={loading} />
             </aside>
           </section>
           <section>{typeof data !== 'undefined' && <CardGrid cardData={data} />}</section>
           <section className="flex justify-center items-center lg:hidden">
-            <Pagination totalPages={totalPages}/>
+            <Pagination totalPages={totalPages} loading={loading}/>
           </section>
         </>
       </Page>
