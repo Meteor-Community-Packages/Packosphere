7fa23118fbe3a5e7a0392b6aa11a44b2ffea1622
Kelly Copley <copleykj@gmail.com>
2021-02-19 22:06:11 -0500

    alternate method to publish email automagically

---
+++ declarations/meteor.d.ts
@@ -0,0 +1,7 @@
+declare module 'meteor/accounts-base' {
+  module Accounts {
+    var _defaultPublishFields: {
+      projection: any
+    };
+  }
+}
--- server/main.ts
+++ server/main.ts
@@ -1,24 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { PackageServer } from 'meteor/peerlibrary:meteor-packages';
 
 import './modules/bots';
 import './modules/accounts';
 import './api';
 
-Meteor.startup(() => {
-  Meteor.publish(null, function () {
-    if (this.userId !== null) {
-      return Meteor.users.find(
-        { _id: this.userId },
-        {
-          fields: {
-            'services.meteor-developer.emails.address': 1,
-          },
-        });
-    } else {
-      return this.ready();
-    }
-  }, { is_auto: true });
+// Tell Meteor to publish the meteor-developer email address so that fast render sends it with html
+Accounts._defaultPublishFields.projection = { 'services.meteor-developer.emails.address': 1, ...Accounts._defaultPublishFields.projection };
 
+Meteor.startup(() => {
   PackageServer.startSyncing();
 });
